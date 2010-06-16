2f6b38985c9cca199ca6a874d6fcc736935e6d11
Pavel Vaynerman <pv@etersoft.ru>
2010-06-17 02:14:19 +0400

    1) переделал формат для хранения настройек CAN. Теперь настройки хранятся не в отдельной секции. А (так же как i/o в <nodes>). Правда теперь не очевидно (только в редакторе) сколько у нас CAN-сетей (т.к. эта информация раскидана по <nodes>) 2) Добавил обработку необходимых полей

--- can_main.py
+++ can_main.py
@@ -12,11 +12,12 @@ class CANMain(gtk.TreeView):
     def __init__(self, conf):
 
         self.conf = conf
+        self.netlist = [] # list of pair [name,tree iter]
 
         gtk.TreeView.__init__(self)
         self.model = None
         self.modelfilter = None
-        #                          Name | Parameters | xmlnode | element type | parent iterator | node_xmlnode
+        #                          Name | Parameters | can_xmlnode | element type | parent iterator | node_xmlnode
         self.model = gtk.TreeStore(gobject.TYPE_STRING,gobject.TYPE_STRING,object,gobject.TYPE_STRING,object,object)
         self.modelfilter = self.model.filter_new()
 
@@ -47,6 +48,11 @@ class CANMain(gtk.TreeView):
         i1.connect("activate", self.on_remove_net_activate)
         i1.show() 
         self.net_popup.append(i1) 
+        
+        i5 = gtk.MenuItem( _("add new node") ) 
+        i5.connect("activate", self.on_add_node_activate)
+        i5.show() 
+        self.net_popup.append(i5) 
 
         self.node_popup = gtk.Menu() 
         i2 = gtk.MenuItem( _("edit node") ) 
@@ -67,21 +73,12 @@ class CANMain(gtk.TreeView):
         vb = gtk.VBox()
         hb = gtk.HBox()
 
-        lb10 = gtk.Label(_("          Node: "))
-        lb10.show()
+        # node label
         self.node_name = gtk.Label()
         self.node_name.show()
-        hb0 = gtk.HBox()
-        self.btn_selnode = gtk.Button("...")
-        self.btn_selnode.connect("clicked", self.on_node_select)
-        self.btn_selnode.show()
-        hb0.pack_start(lb10,False,False,3)
-        hb0.pack_start(self.node_name,True,True,3)
-        hb0.pack_start(self.btn_selnode,False,False,3)
-        hb0.show()
-        vb.pack_start(hb0,True,True,3)
+        vb.pack_start(self.node_name,True,True,3)
 
-        lb1 = gtk.Label(_("CAN NodeID: "))
+        lb1 = gtk.Label(_("NodeID: "))
         lb1.show()
         self.node_id = gtk.Entry()
         self.node_id.set_width_chars(5)
@@ -93,7 +90,7 @@ class CANMain(gtk.TreeView):
         hb1.show()
         vb.pack_start(hb1,True,True,3)
 
-        lb11 = gtk.Label(_("      eds file: "))
+        lb11 = gtk.Label(_("eds file: "))
         lb11.show()
         self.eds = gtk.Entry()
         self.eds.set_width_chars(20)
@@ -105,12 +102,12 @@ class CANMain(gtk.TreeView):
         hb2.show()
         vb.pack_start(hb2,True,True,3)
 
-        lbl2 = gtk.Label(_("      hbSensor: "))
+        lbl2 = gtk.Label(_("hbSensor: "))
         lbl2.show()
         self.hb_sensor = gtk.Label()
         self.hb_sensor.show()
         self.btn_sel_hbsensor = gtk.Button("...")
-        self.btn_sel_hbsensor.connect("clicked", self.on_node_select)
+        self.btn_sel_hbsensor.connect("clicked", self.on_sensor_select)
         self.btn_sel_hbsensor.show()
         hb3 = gtk.HBox()
         hb3.pack_start(lbl2,False,False,3)
@@ -119,37 +116,98 @@ class CANMain(gtk.TreeView):
         hb3.show()
         vb.pack_start(hb3,True,True,3)
 
+        lbl4 = gtk.Label(_("NodeRespond: "))
+        lbl4.show()
+        self.respond = gtk.Label()
+        self.respond.show()
+        self.btn_respond = gtk.Button("...")
+        self.btn_respond.connect("clicked", self.on_sensor_select)
+        self.btn_respond.show()
+        hb4 = gtk.HBox()
+        hb4.pack_start(lbl4,False,False,3)
+        hb4.pack_start(self.respond,True,True,3)
+        hb4.pack_start(self.btn_respond,False,False,3)
+        hb4.show()
+        vb.pack_start(hb4,True,True,3)
+
+        lbl5 = gtk.Label(_("Respond1: "))
+        lbl5.show()
+        self.respond1 = gtk.Label()
+        self.respond1.show()
+        self.btn_respond1 = gtk.Button("...")
+        self.btn_respond1.connect("clicked", self.on_sensor_select)
+        self.btn_respond1.show()
+        hb5 = gtk.HBox()
+        hb5.pack_start(lbl5,False,False,3)
+        hb5.pack_start(self.respond1,True,True,3)
+        hb5.pack_start(self.btn_respond1,False,False,3)
+        hb5.show()
+        vb.pack_start(hb5,True,True,3)
+
+        lbl6 = gtk.Label(_("Respond2: "))
+        lbl6.show()
+        self.respond2 = gtk.Label()
+        self.respond2.show()
+        self.btn_respond2 = gtk.Button("...")
+        self.btn_respond2.connect("clicked", self.on_sensor_select)
+        self.btn_respond2.show()
+        hb6 = gtk.HBox()
+        hb6.pack_start(lbl6,False,False,3)
+        hb6.pack_start(self.respond2,True,True,3)
+        hb6.pack_start(self.btn_respond2,False,False,3)
+        hb6.show()
+        vb.pack_start(hb6,True,True,3)
+
         vb.show()
         
         self.dlg_can = gtk.Dialog(_("Setup CAN node"),None,gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,(gtk.STOCK_OK,gtk.RESPONSE_OK,gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL)) 
         self.dlg_can.vbox.pack_start(vb,True,True,0)
 
+        self.dlg_name = gtk.Dialog(_("Set name"),None,gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,(gtk.STOCK_OK,gtk.RESPONSE_OK,gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL)) 
+        lb110 = gtk.Label(_("Name: "))
+        lb110.show()
+        self.netname = gtk.Entry()
+        self.netname.set_width_chars(20)
+        self.netname.set_max_length(80)
+        self.netname.show()
+        hb10 = gtk.HBox()
+        hb10.pack_start(lb110,False,False,3)
+        hb10.pack_start(self.netname,True,True,3)
+        hb10.show()
+        self.dlg_name.vbox.pack_start(hb10,True,True,0)
+
         self.show_all()
-
+       
     def build_tree(self):
-        node = self.conf.xml.findNode(self.conf.xml.getDoc(),"CAN_Net")[0].children.next 
-        while node != None:
-            iter1 = self.model.append(None,[node.prop("name"),node.prop("comment"),node,"net",None,None])
-            self.read_nodes(node,iter1)
-            node = self.conf.xml.nextNode(node)
-
-    def read_nodes(self,rootnode,iter):
-        node = self.conf.xml.getNode(rootnode.children)
-        while node != None:
-            info  = ' eds=' + str(node.prop("eds"))
-            info  = info + ' nodeID=' + str(node.prop("nodeID"))
-            info  = info + '...'
-            iter2 = self.model.append(iter, [node.prop("name"),info,node,"node",iter,self.find_node(node.prop("name"))])
-            node = self.conf.xml.nextNode(node)
-
-    def find_node(self, node_name):
         node = self.conf.xml.findNode(self.conf.xml.getDoc(),"nodes")[0].children.next 
         while node != None:
-            if node.prop("name") == node_name:
-                return node
-            node = self.conf.xml.nextNode(node)           
-        return None
+             cannode = self.conf.xml.findNode(node,"can")
+             if cannode[0] != None:
+                 cnode = cannode[0].children.next
+             while cnode != None:
+                   self.add_net(cnode,node)
+                   cnode = self.conf.xml.nextNode(cnode)
+
+             node = self.conf.xml.nextNode(node)
     
+    def add_net(self,cannode, node):
+         name = cannode.prop("net")
+         for n in self.netlist:
+               if n[0] == name:
+                    self.add_node(cannode,node,n[1])
+                    return False
+         
+         iter = self.model.append(None,[name,"",None,"net",None,None])
+         self.netlist.append([name,iter])
+         self.add_node(cannode,node,iter)
+         return True
+
+    def add_node(self,cannode,node,iter):
+            info  = ' eds=' + str(cannode.prop("eds"))
+            info  = info + ' nodeID=' + str(cannode.prop("nodeID"))
+            info  = info + '...'
+            self.model.append(iter,[node.prop("name"),info,cannode,"node",iter,node])
+
     def on_button_press_event(self, object, event):                                                                                                                                 
 #        print "*** on_button_press_event"
         (model, iter) = self.get_selection().get_selected()
@@ -176,6 +234,15 @@ class CANMain(gtk.TreeView):
         return False
 
     def on_add_net_activate(self, menuitem):
+        self.netname.set_text("")
+        res = self.dlg_name.run()
+        self.dlg_name.hide()
+        if res != gtk.RESPONSE_OK:
+            return
+
+        name = self.netname.get_text()
+        iter = self.model.append(None,[name,"",None,"net",None,None])
+        self.netlist.append([name,iter])
         self.conf.mark_changes()
         pass
 
@@ -206,33 +273,43 @@ class CANMain(gtk.TreeView):
 
         self.eds.set_text( self.init_text_param(cnode,"eds") )
         self.node_id.set_text( self.init_text_param(cnode,"nodeID") )
-        self.node_name.set_text( self.init_text_param(cnode,"name") )
+        self.node_name.set_text( self.init_text_param(node_xmlnode,"name") )
         self.hb_sensor.set_text( self.init_text_param(cnode,"hbSensor") )
+        self.respond.set_text( self.init_text_param(cnode,"respond") )
+        self.respond1.set_text( self.init_text_param(cnode,"respond1") )
+        self.respond2.set_text( self.init_text_param(cnode,"respond2") )
         
         res = self.dlg_can.run()
         self.dlg_can.hide()
         if res != gtk.RESPONSE_OK:
             return
 
-#        cnode.setProp("card",str(self.card_num.get_value_as_int()))
-#        cnode.setProp("baddr",self.card_ba.get_text())
+        cnode.setProp("eds",self.eds.get_text())
+        cnode.setProp("nodeID",self.node_id.get_text())
+        cnode.setProp("hbSensor",self.hb_sensor.get_text())
+        cnode.setProp("respond",self.respond.get_text())
+        cnode.setProp("respond1",self.respond1.get_text())
+        cnode.setProp("respond2",self.respond2.get_text())
 
-#        info  = 'card=' + str(cnode.prop("card"))
-#        info  = info + ' BA=' + str(cnode.prop("baddr"))
-#        model.set_value(iter,1,info)
+        info  = ' eds=' + str(cnode.prop("eds"))
+        info  = info + ' nodeID=' + str(cnode.prop("nodeID"))
+        info  = info + '...'
+        model.set_value(iter,1,info)
         self.conf.mark_changes()
 
-    def on_node_select(self, btn):
-        if btn == self.btn_selnode:
-             print "****** node select"
-             self.conf.dlg_nodes.set_select_name(self.node_name.get_text())
-             node = self.conf.dlg_nodes.run(self)
-             if node != None:
-                 self.node_name.set_text(node.prop("name"))
-        elif btn == self.btn_sel_hbsensor:
-             print "****** hb sensor select "
-             self.conf.dlg_slist.set_selected_name(self.hb_sensor.get_text())
-             snode = self.conf.dlg_slist.run(self)
-             if snode != None:
-                 self.hb_sensor.set_text(snode.prop("name"))
+    def on_sensor_action(self, lbl):
+        self.conf.dlg_slist.set_selected_name(lbl.get_text())
+        snode = self.conf.dlg_slist.run(self)
+        if snode != None:
+            lbl.set_text(snode.prop("name"))
+  
+    def on_sensor_select(self, btn):
+        if btn == self.btn_sel_hbsensor:
+            self.on_sensor_action(self.hb_sensor)
+        if btn == self.btn_respond:
+            self.on_sensor_action(self.respond)
+        if btn == self.btn_respond1:
+            self.on_sensor_action(self.respond1)
+        if btn == self.btn_respond2:
+            self.on_sensor_action(self.respond2)
          
