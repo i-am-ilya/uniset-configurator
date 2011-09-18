5e2fe841792344be7c6cb5cfad24eda837fc34ac
Pavel Vaynerman <pv@etersoft.ru>
2011-09-19 03:19:57 +0400

    (ioeditor): добавил DO32

---
+++ modules/uniset_io/cards/card_DO32.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import gtk
+import gobject
+import UniXML
+import simple_card
+from global_conf import *
+
+class Card_DO32(simple_card.SimpleCard):
+
+    def __init__(self,datdir,uifile="card_None.ui"):
+
+        simple_card.SimpleCard.__init__(self,datdir,uifile)
+
+        self.cname = "DO32"
+        self.clist = self.build_channel_list(self.cname)
+
+        self.node_xmlnode = None
+        self.editor_ui = None
+
+        self.module_name = "do32"
+        self.default_init(self.cname)
+
+    def get_supported_cards( self ):
+        return ["DO32"]
+
+    def build_channel_list( self, cname ):
+        clist=[]
+        for i in range(0,16):
+            clist.append([i,"J1:"+str(i),"DO",0])
+        for i in range(16,32):
+            clist.append([i,"J2:"+str(i-16),"DO",0])
+
+        return clist
+
+    def get_params(self,cnode):
+        return ""
+
+def create_editor(datdir):
+    return Card_DO32(datdir)
+
+def editor_name():
+    return "Card_DO32"
